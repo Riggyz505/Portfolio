c5103a936e33b617638a6cf46df2fa442ef42512
Ethan Zeronik <ejzeronik@gmail.com>
2023-05-07 15:42:59 -0400

    I am stupid, renamed a file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  vite: {
-    ssr: {
-      noExternal: ['open-props'],
-    }
-  },
 });
