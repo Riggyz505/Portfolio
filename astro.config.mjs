5a1e49b12a802a74614e0ceb11a6ae7bd7c60449
Ethan Zeronik <ejzeronik@gmail.com>
2023-05-06 23:29:42 -0400

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
+import css from "rollup-plugin-import-css";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    build: {
+      rollupOptions: {
+        input: "index.js",
+        output: { file: "dist/index.js", format: "esm" },
+        plugins: [css()],
+      },
+    },
+  },
 });
