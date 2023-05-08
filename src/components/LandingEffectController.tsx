f63ff321601f63be8bcb30293910b63db7271403
Ethan Zeronik <ejzeronik@gmail.com>
2023-05-07 23:42:46 -0400

    I made a cool landing page

---
+++ src/components/LandingEffectController.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+import Stars from "./landingEffects/Stars"
+
+const seed = 0;
+
+
+export default function LandingEffectController() {
+    return <>
+        <Stars />
+    </>
+}
